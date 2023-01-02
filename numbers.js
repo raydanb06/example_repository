5808efbcb3807b15509d76a99066f189c233cc74
Raydan Bala <raydanb@gmail.com>
2023-01-01 23:36:30 -0800

    .toString(base) method on strings

--- numbers.js
+++ numbers.js
@@ -25,3 +25,15 @@ console.log(`Binary number: ${0b11111111}`);
 
 // Octal
 console.log(`Octal number: ${0o377}`);
+
+/* toString(base): The method num.toString(base) returns a string representation of num in the numeral system with the given base.*/
+let num = 255;
+
+console.log(`.toString no base given: ${num.toString()}`);
+console.log(`.toString base 16 given (hex): ${num.toString(16)}`);
+console.log(`.toString base 2 given (bitwise): ${num.toString(2)}`);
+// two dots to call a method directly on a number
+console.log(`..toString base 36 given (numeral): ${123456..toString(36)}`);
+// parenthesis to call a method dirctly on a number
+console.log(`().toString base 36 given (numeral): ${(123456).toString(36)}`);
+
