a6c1bc802892e44590c30ae418b14ff53f6de780
Raydan Bala <raydanb@gmail.com>
2023-01-01 23:10:06 -0800

    Appending a number with e, eg: 1e9 equals 1 billion, 1e-6 equals micro.

---
+++ numbers.js
@@ -0,0 +1,16 @@
+let billion = 1e9;
+alert(7.3e9);
+
+if (billion === 1e9) {
+  alert(`1e9 is equal to ${billion}`);
+} else {
+  alert(`1e9 does not equal ${billion}`);
+}
+
+let mcs = 0.000001;
+
+if (mcs === 1e-6) {
+  alert(`1e-6 is equal to ${mcs}`);
+} else {
+  alert(`1e-6 does not equal ${mcs}`)
+}
