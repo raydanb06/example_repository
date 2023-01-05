92a5fcc9ddd78b41b03d0f956cc2d5e3d968e882
Raydan Bala <raydanb@gmail.com>
2023-01-05 12:13:56 -0800

    String length, Accessing characters, Strings are immutable

--- strings.js
+++ strings.js
@@ -29,4 +29,40 @@ console.log(str1 === str2);
 \' \" \` - quotes
 \\ - backslash
 \t - tab
-\b, \f, \v - backspace, form feed, vertical tab */
+\b, \f, \v - backspace, form feed, vertical tab 
+\ - escape character*/
+console.log('I\'m the Walrus!'); // using backslash
+console.log("I'm the Walrus!");
+
+// String length - the length property has the string length
+console.log(`My\n`.length); // \n is a single 'special' character, counts as 1
+
+// Accessing characters - use [pos] or call method str.at(pos), starts at 0
+let str = `Hello`;
+
+// first character
+console.log(str[0]);
+console.log(str.at(0));
+
+// last character
+console.log(str[str.length - 1]);
+console.log(str.at(-1));
+
+// traverse a string backwards
+for (let i = str.length - 1; i >= 0; i--) {
+  console.log(str[i]);
+}
+
+// can use for..of to iterate over characters
+for (let char of 'Hello') {
+  console.log(char);
+}
+
+// Strings are immutable - Strings can't be changed in Javascript
+let str3 = 'Hi';
+str3[0] = 'h'; // error
+console.log(str3[0]); // still shows H
+// usual workaround is to create a whole new string and assign it to str
+let str4 = 'Hi'
+str4 = 'h' + str4[1];
+console.log(str4);
