ebebf62b89feeeb5146da446e573bc9fce1689af
Raydan Bala <raydanb@gmail.com>
2023-01-09 23:51:23 -0800

    Task 3 -Truncate the text

--- strings.js
+++ strings.js
@@ -197,3 +197,11 @@ function checkSpam(str) {
 console.log(checkSpam('buy ViAgRA now'));
 console.log(checkSpam('free xxxxx'));
 console.log(checkSpam("innocent rabbit"));
+
+// Task 3 -Truncate the text
+function truncate(str, maxlength) {
+  let truncateStr = '';
+  return (str.length > maxlength) ? str.slice(0, maxlength - 1) + '...' : str;
+}
+console.log(truncate("What I'd like to tell on this topic is:", 20));
+console.log(truncate("Hi everyone!", 20));
